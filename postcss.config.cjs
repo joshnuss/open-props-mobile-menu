4c66d42b70822f3f5fb2359a220a101c658f4f02
Josh Nussbaum <joshnuss@gmail.com>
2024-05-10 03:08:14 -0400

    Basic postcss setup

---
+++ postcss.config.cjs
@@ -0,0 +1,16 @@
+const presetEnv = require('postcss-preset-env')
+
+const config = {
+	plugins: [
+		presetEnv({
+			minimumVendorImplementations: 2,
+			features: {
+				'nesting-rules': true,
+				'custom-media-queries': true,
+				'media-query-ranges': true
+			}
+		})
+	]
+}
+
+module.exports = config
